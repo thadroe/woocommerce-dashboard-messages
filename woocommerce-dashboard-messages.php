cac4ed25e6c2260839a9750f8ef2ea9a81f05c9f
Thad Roe <git@thadroe.com>
2017-08-07 07:16:14 -0500

    Save meta input

--- woocommerce-dashboard-messages.php
+++ woocommerce-dashboard-messages.php
@@ -37,6 +37,27 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters('active_plugins', ge
 
                     <?php
                 }
+
+                // Save the custom meta input
+                function dashboard_meta_save( $post_id ) {
+                
+                    // Checks save status
+                    $is_autosave = wp_is_post_autosave( $post_id );
+                    $is_revision = wp_is_post_revision( $post_id );
+                    $is_valid_nonce = ( isset( $_POST[ 'dashboard_nonce' ] ) && wp_verify_nonce( $_POST[ 'dashboard_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+                
+                    // Exits script depending on save status
+                    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+                        return;
+                    }
+                
+                    // Checks for input and sanitizes/saves if needed
+                    if( isset( $_POST[ 'meta-text' ] ) ) {
+                        update_post_meta( $post_id, 'meta-text', sanitize_text_field( $_POST[ 'meta-text' ] ) );
+                    }
+                
+                }
+                add_action( 'save_post', 'dashboard_meta_save' );
             }
 
             // Create settings tab
